f223d10611e671d88aeebbc077516ce833287bdd
Aleks <bla3e316@gmail.com>
2018-01-25 16:06:55 +0200

    compleate basic task

--- pages/index.js
+++ pages/index.js
@@ -9,23 +9,33 @@ export default class Home extends React.Component {
   handleSubmin = e => {
     e.preventDefault()
 
-    const {username} = this.setState
+    const {username} = this.state
 
-    fetch('https://api.github.com/users/${username}')
+    fetch(`https://api.github.com/users/${username}`)
       .then(res => res.json())
       .then(user => this.setState({user}))
       .catch(err => console.error(err))
   }
 
     render() {
+      const {user} = this.state
+
         return (
             <div>
+              {user && (
+                <div>
+                  <img src={user.avatar_url} alt={user.login} width={50}/>
+                  <h3>{user.name}</h3>
+                  <p>{user.company}</p>
+                </div>
+              )}
               <form onSubmit={this.handleSubmin}>
                 <input
                   type="text"
                   placeholder="Type GitHub username..."
                   onChange={e => this.setState({ username: e.target.value })}
                 />
+                <button type="submit">Go!</button>
               </form>
             </div>
         )
